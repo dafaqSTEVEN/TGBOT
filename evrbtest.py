ee524708a960a8fd8e7c725805eae959dc897e26
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-02-13 19:22:19 +0800

    yo.exe is responding

--- evrbtest.py
+++ evrbtest.py
@@ -1,7 +1,20 @@
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Introduction-to-the-API
 
 import telegram
-from credentials import ChingDim
+from telegram.ext import Updater
 
-bot = telegram.Bot(token=ChingDim.token)
-print(bot.get_me())
+from credentials import ChingDim
+from telegram.ext import CommandHandler
+
+updater = Updater(token=ChingDim.token)
+dispatcher = updater.dispatcher
+
+
+def yo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('yo', yo)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
